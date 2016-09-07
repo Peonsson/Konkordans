a9728edeb1031ab54691b21510b061462ab10c9a
Johan Pettersson <xsnudelx@hotmail.com>
2016-09-07 22:15:42 +0200

    added serializing on hashtable. sped up loop a bit using indexOf instead of split()

--- src/main/java/Konkordans.java
+++ src/main/java/Konkordans.java
@@ -1,55 +1,65 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
 import java.util.Hashtable;
 
-public class Konkordans {
+public class Konkordans implements Serializable {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-        RandomAccessFile randomAccessFile = null;
-        try {
-            //randomAccessFile = new RandomAccessFile("/Users/Peonsson/WORK/tokenizer", "r");
-            randomAccessFile = new RandomAccessFile("C:\\index", "r");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        /*
+            Read hashtable from disk.
+            NOTE: will fail if hashtable haven't been previously written to disk.
+         */
+//        FileInputStream fileInputStream = new FileInputStream("hashtable.dat");
+//        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+//        Hashtable<String, Long> hashtableFromDisk = (Hashtable<String, Long>) objectInputStream.readObject();
+//        objectInputStream.close();
+//        fileInputStream.close();
+//        System.out.println("hashtableFromDisk size: " + hashtableFromDisk.size());
+//        System.out.println(hashtableFromDisk.toString());
 
-        String word = "hej";
-        String line = "init";
+
+        double start = System.currentTimeMillis();
+        //randomAccessFile = new RandomAccessFile("/Users/Peonsson/WORK/tokenizer", "r");
+        RandomAccessFile randomAccessFile = new RandomAccessFile("C:\\index", "r");
+
+        String word;
         String prev = null;
         long position = 0;
-        Hashtable<String, Long> numbers = new Hashtable<>(30 * 30 * 30);
-        String[] strings;
-        double start = System.currentTimeMillis();
+        Hashtable<String, Long> numbers = new Hashtable<>(12000);
+        StringBuffer sb = new StringBuffer();
+        String line = "init";
+        while (line != null) {
 
-        while(line != null) {
+            position = randomAccessFile.getFilePointer();
+            if ((line = randomAccessFile.readLine()) == null)
+                break;
 
-            try {
-                position = randomAccessFile.getFilePointer();
-                if((line = randomAccessFile.readLine()) == null)
+            for (int i = 0; i < 3; i++) {
+                if (line.charAt(i) == ' ')
                     break;
-
-                strings = line.split(" ");
-                word = strings[0];
-
-                if (word.length() > 3)
-                    word = word.substring(0, 3);
-
-                if(word.equals(prev))
-                    continue;
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+                sb.append(line.charAt(i));
             }
-            prev = word;
+            word = sb.toString();
+            sb.setLength(0);
 
+            if (word.equals(prev))
+                continue;
+
+            prev = word;
             numbers.put(word, position);
         }
-        //System.out.println(numbers.toString());
+
         System.out.println("numbers size: " + numbers.size());
         System.out.println((System.currentTimeMillis() - start) / 1000);
+
+        /*
+            Write hashtable to disk.
+         */
+//        FileOutputStream fileOut = new FileOutputStream("hashtable.dat");
+//        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+//        out.writeObject(numbers);
+//        out.close();
+//        fileOut.close();
+//        System.out.printf("saved hashtable in hashtable.dat");
     }
 }
